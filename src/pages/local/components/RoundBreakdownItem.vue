f3512cc88226c3723e77312eb2525f85523027f7
MarcinSkic <marcinskicoficjal@gmail.com>
2026-05-07 20:47:23 +0200

    feat: move preview to left in round breakdown

--- src/pages/local/components/RoundBreakdownItem.vue
+++ src/pages/local/components/RoundBreakdownItem.vue
@@ -75,7 +75,7 @@ const { blobUrl: previewBlobUrl } = useLinkPreview(previewUrl)
         v-if="previewBlobUrl"
         :src="previewBlobUrl"
         alt="Preview"
-        class="w-1/2 object-cover"
+        class="w-1/2 object-cover object-left"
       >
     </div>
   </div>
